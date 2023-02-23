96aab908c79c511ac0116c80004ffab74fa43712
Nursultan Dulatov <80988817+Twoeyes13@users.noreply.github.com>
2023-02-24 02:16:06 +0600

    Too long time thinking about super in construction

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,20 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String aNo = this.hasFur ? "a" : "no";
+        String paws = this.numberOfPaws < 2 ? "paw" : "paws";
+        return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + " "+ paws + " and " + aNo + " fur.";
+        //"This animal is mostly (color). It has (numberOfPaws) paws and ('a'/'no' -> depends on value of hasFur) fur."
+    }
 }
---
+++ src/main/java/com/epam/mjc/stage0/Main.java
@@ -0,0 +1,9 @@
+package com.epam.mjc.stage0;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog doge = new Dog();
+        doge.getDescription();
+        System.out.println(doge.getDescription());
+    }
+}
